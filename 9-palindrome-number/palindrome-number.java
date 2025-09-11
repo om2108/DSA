1e77321820d33a39b7b97c2e7135bea5ff7c8481
Omkar Pawar <pawarnandkumaromved@gmail.com>
2025-09-11 19:58:24 +0530

    Time: 6 ms (25.83%) | Memory: 43.8 MB (89.31%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        for (int i=0; i<s.length()-1; i++){
+            if (s.charAt(i)!=s.charAt(s.length()-i-1)){
+                return false;
+            }
+        }return true;
+    }
+}
