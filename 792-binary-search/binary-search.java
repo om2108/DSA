559ad726d954c04df38b1f313c03bb79eb2f565d
Omkar Pawar <pawarnandkumaromved@gmail.com>
2026-06-15 08:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (49.48%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start=0, end=arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start) / 2;
+            if(arr[mid] > target){
+                end = mid -1;
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
