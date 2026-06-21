9db3c2d27da1b3ad736a2a15d3dcdbdfda146d40
Omkar Pawar <pawarnandkumaromved@gmail.com>
2026-06-21 10:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (38.34%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,59 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr){
+        int peak = peakIndexInMountainArray(mountainArr);
+        int firstTry = orderAgnosticBS(mountainArr, target, 0, peak);
+        if(firstTry != -1){
+            return firstTry;
+        }
+        return orderAgnosticBS(mountainArr, target, peak+1, mountainArr.length()-1);
+    }
+    public int peakIndexInMountainArray(MountainArray mountainArr){
+        int start = 0;
+        int end = mountainArr.length() - 1;
+        
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            if(mountainArr.get(mid) > mountainArr.get(mid + 1)){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    public int orderAgnosticBS(MountainArray mountainArr, int target, int start, int end){
+        
+        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(mountainArr.get(mid) == target){
+                return mid;
+            }
+            
+            if(isAsc){
+               if(target < mountainArr.get(mid)){
+                   end = mid - 1;
+               } else{
+                   start = mid + 1;
+               }
+            }else{
+                if(target > mountainArr.get(mid)){
+                   end = mid - 1;
+               } else{
+                   start = mid + 1;
+               }
+            }
+        }
+        return -1;
+    }
+}
